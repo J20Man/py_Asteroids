28441415a6707c7dbf9a6ca5ebdaab4c751e7954
J20Man <josephttwentyman@hotmail.co.uk>
2025-10-05 16:49:43 +0100

    Adding the circle obj class

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import pygame
 from constants import *
 
+#Class for the in game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        #leave blank to allow children to overwrite
+        pass
+
+    def update(self, dt):
+        #as above, children will overwrite
+        pass
+
+
+
+
 def main():
 
     pygame.init()
